38dc6dbacf2f22b59a0a2fbb2271e702a4432d8e
Aziza <aziza.helmy35@gmail.com>
2023-07-04 11:12:50 +0300

    add required dependencies and setup configuration

--- app/src/main/java/com/example/triviagame/MainActivity.kt
+++ app/src/main/java/com/example/triviagame/MainActivity.kt
@@ -16,31 +16,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            TriviaGameTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TriviaGameTheme {
-        Greeting("Android")
     }
 }
