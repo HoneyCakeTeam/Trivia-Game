315302980eed46c6feb6b19772919c93126727d7
Aziza <aziza.helmy35@gmail.com>
2023-07-04 12:09:36 +0300

    add fake classes to push the pkgs "D

---
+++ app/src/main/java/com/example/triviagame/data/Fake.kt
@@ -0,0 +1,7 @@
+package com.example.triviagame.data
+
+/**
+ * Created by Aziza Helmy on 7/4/2023.
+ */
+class Fake {
+}
---
+++ app/src/main/java/com/example/triviagame/di/Fake.kt
@@ -0,0 +1,7 @@
+package com.example.triviagame.di
+
+/**
+ * Created by Aziza Helmy on 7/4/2023.
+ */
+class Fake {
+}
