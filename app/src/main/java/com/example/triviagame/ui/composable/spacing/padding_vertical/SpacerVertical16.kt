a353b45355075d4dbc5cd590ee4363df8c871cbc
Mennaakram <mennaakram976@gmail.com>
2023-07-05 01:21:42 +0300

    add: vertical padding

---
+++ app/src/main/java/com/example/triviagame/ui/composable/spacing/padding_vertical/SpacerVertical16.kt
@@ -0,0 +1,12 @@
+package com.example.triviagame.ui.composable.spacing.padding_vertical
+
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun SpacerVertical16() {
+    Spacer(modifier = Modifier.height(16.dp))
+}
