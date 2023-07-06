ee1de223d1ea6d12078060a671f2259b8d9f5f6d
Mohamed Naser <mahmmedn19@gmail.com>
2023-07-07 01:47:34 +0300

    remove unnecessary imports

--- app/src/main/java/com/example/triviagame/ui/screens/GameResultScreen.kt
+++ app/src/main/java/com/example/triviagame/ui/screens/GameResultScreen.kt
@@ -1,6 +1,5 @@
 package com.example.triviagame.ui.screens
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
