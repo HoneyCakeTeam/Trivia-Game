7ca52e9ad52a320bc1c0c6378a1d200602977260
Mennaakram <mennaakram976@gmail.com>
2023-07-07 22:43:10 +0300

    add: shapes

---
+++ app/src/main/java/com/example/triviagame/ui/theme/Shapes.kt
@@ -0,0 +1,13 @@
+package com.example.triviagame.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+
+val Shape = Shapes(
+     extraLarge = RoundedCornerShape(28.dp),
+     large = RoundedCornerShape(24.dp),
+     medium = RoundedCornerShape(16.dp),
+     small = RoundedCornerShape(12.dp),
+     extraSmall = RoundedCornerShape(8.dp)
+)
