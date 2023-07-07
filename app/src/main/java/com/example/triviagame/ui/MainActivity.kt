09377117fd80d7d99d91d84e99ef572b53b82c94
Mohamed-Shaban <ms2951@fayoum.edu.eg>
2023-07-07 07:49:02 +0300

    implement splash screen

--- app/src/main/java/com/example/triviagame/ui/MainActivity.kt
+++ app/src/main/java/com/example/triviagame/ui/MainActivity.kt
@@ -3,10 +3,12 @@ package com.example.triviagame.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
           TriviaApp()
         }
