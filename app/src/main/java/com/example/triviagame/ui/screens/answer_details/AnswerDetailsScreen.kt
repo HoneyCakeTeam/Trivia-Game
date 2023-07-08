3e16a3ca72d2ef16e4499bb4ba9daa0df97491c0
Asia sama <sehunexo710@gmail.com>
2023-07-08 12:01:14 +0300

    add background

--- app/src/main/java/com/example/triviagame/ui/screens/answer_details/AnswerDetailsScreen.kt
+++ app/src/main/java/com/example/triviagame/ui/screens/answer_details/AnswerDetailsScreen.kt
@@ -12,6 +12,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -88,7 +91,10 @@ fun AnswerDetailsContent() {
             onBack = {})
         Box(
             modifier = Modifier
-                .background(color = Primary)
+                .paint(
+                    painter = painterResource(id = R.drawable.background),
+                    contentScale = ContentScale.Crop
+                )
                 .padding(top = 100.dp, bottom = 150.dp)
                 .align(Alignment.Center)
         ) {
