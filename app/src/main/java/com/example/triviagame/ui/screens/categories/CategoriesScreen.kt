bc3acf2259064e55d5c1514b67c8de7e1151993f
Nour Elden Alaa <nourelden515@gmail.com>
2023-07-10 15:09:12 +0300

    fix merge conflicts

--- app/src/main/java/com/example/triviagame/ui/screens/categories/CategoriesScreen.kt
+++ app/src/main/java/com/example/triviagame/ui/screens/categories/CategoriesScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.rememberBottomSheetScaffoldState
 import androidx.compose.material.rememberBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
