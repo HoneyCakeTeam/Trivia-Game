8681b41f68d9cf27626b3baae60c2b51507f2241
Aziza <aziza.helmy35@gmail.com>
2023-07-13 03:59:17 +0300

    rename classes and refactor

--- app/src/main/java/com/example/triviagame/data/source/remote/network/TriviaService.kt
+++ app/src/main/java/com/example/triviagame/data/source/remote/network/TriviaService.kt
@@ -1,10 +1,10 @@
 package com.example.triviagame.data.source.remote.network
 
-import com.example.triviagame.data.source.remote.model.QuestionModel
+import com.example.triviagame.data.source.remote.model.QuestionDto
 
 interface TriviaService {
     suspend fun getTriviaQuestions(
         category: String,
         difficulty: String
-    ): List<QuestionModel>
+    ): List<QuestionDto>
 }
--- app/src/main/java/com/example/triviagame/ui/screens/play/PlayUiState.kt
+++ app/src/main/java/com/example/triviagame/ui/screens/play/PlayUiState.kt
@@ -1,20 +1,27 @@
 package com.example.triviagame.ui.screens.play
 
+import com.example.triviagame.data.source.remote.model.QuestionDto
 import com.example.triviagame.ui.screens.answer_details.AnswerUiState
 
 data class PlayUiState(
-    val questions: List<QuestionUiState> = emptyList(),
+    val questions: List<QuestionUiState> = emptyList(),//TODO NOT USED
     val numberOfQuestions: Int = 0,
     val currentQuestionIndex: Int = 0,
     val userScore: Int = 0,
 )
+
 data class QuestionUiState(
     val question: String = "",
     val answers: List<AnswerUiState> = emptyList(),
     val correctAnswer: String = "",
+    val incorrectAnswers: List<String> = emptyList(),
     val selectedAnswer: String = "",
     val isAnswered: Boolean = false,
     val isCorrect: Boolean = false,
     val questionNumber: Int = 0,
-    val timer: Long=30000L
+    val timer: Long = 30000L
 )
+
+fun QuestionDto.toQuestionUiState() =
+    QuestionUiState(question = question, incorrectAnswers = incorrectAnswers)
+
