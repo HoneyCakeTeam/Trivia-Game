bddf3c3262a478a1545c1b636dc6a991daed799a
Mennaakram <mennaakram976@gmail.com>
2023-07-13 05:05:29 +0300

    enhance game result screen ui

--- app/src/main/java/com/example/triviagame/ui/composable/answer_details_composable/ResultCard.kt
+++ app/src/main/java/com/example/triviagame/ui/composable/answer_details_composable/ResultCard.kt
@@ -2,6 +2,7 @@ package com.example.triviagame.ui.composable.answer_details_composable
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -9,30 +10,24 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Card
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.triviagame.ui.screens.game_result.composable.TextResult
 import com.example.triviagame.ui.theme.CardBackgroundColor
-import com.example.triviagame.ui.theme.RoundedShape
 import com.example.triviagame.ui.theme.Typography
-import com.example.triviagame.ui.theme.White_FF
-
 
 @Composable
 fun ReusableCard(
     modifier: Modifier = Modifier,
     labelText: String,
     questionCount: String,
-    icon: Int,
+    circleColor: Color,
 ) {
     Card(
         modifier = modifier
@@ -51,59 +46,10 @@ fun ReusableCard(
                 horizontalArrangement = Arrangement.spacedBy(8.dp, Alignment.Start),
                 verticalAlignment = Alignment.CenterVertically
             ) {
-                CustomIcon(icon = icon)
+                Box(modifier = Modifier.size(6.dp).clip(CircleShape).background(circleColor))
                 TextResult(text = labelText, style = Typography.bodyMedium)
             }
             TextResult(text = questionCount, style = Typography.bodyLarge)
         }
     }
-}
-
-@Composable
-fun CustomIcon(
-    modifier: Modifier = Modifier,
-    icon: Int,
-    contentDescription: String? = null,
-    tint: Color = Color.Unspecified,
-) {
-    Icon(
-        modifier = modifier.size(6.dp),
-        painter = painterResource(icon),
-        contentDescription = contentDescription,
-        tint = tint
-    )
-}
-
-@Composable
-fun CustomButton(
-    modifier: Modifier = Modifier,
-    text: String,
-    buttonColor: Color,
-    textColor: Color,
-    onClick: () -> Unit,
-) {
-    Button(
-        modifier = modifier
-            .fillMaxWidth()
-            .height(54.dp),
-        shape = RoundedShape.medium,
-        colors = ButtonDefaults.buttonColors(buttonColor),
-        onClick = onClick,
-    ) {
-        Text(
-            text = text,
-            style = Typography.bodyLarge,
-            color = textColor
-        )
-    }
-}
-
-
-@Preview
-@Composable
-fun Previews() {
-    CustomButton(text = "Play again", onClick = {
-        //TODO PLAY AGAIN
-    }, buttonColor = CardBackgroundColor, textColor = White_FF)
-
 }
