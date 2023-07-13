969a20346c350365e9ed62b1f8ce7a8c6f224be5
Ethaar hussien <ethaar7735@gmail.com>
2023-07-13 19:32:06 +0300

    fix Navigation

--- app/src/main/java/com/example/triviagame/ui/screens/play/PlayRoute.kt
+++ app/src/main/java/com/example/triviagame/ui/screens/play/PlayRoute.kt
@@ -6,7 +6,6 @@ import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavType
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
-import com.example.triviagame.ui.LocalNavigationProvider
 
 private const val ROUTE = "PlayScreen"
 
